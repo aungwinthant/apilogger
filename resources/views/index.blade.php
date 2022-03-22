ce955d0f2f0c59b6876e83da584a64c831273125
Gilbert Paquin <gilbertpaquin1995@gmail.com>
2022-03-22 15:09:08 -0400

    Better GUI
    Full with
    Move information on top
    Payload display with formating

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,80 +13,70 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></head>
-    <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet">
 <body style="font-family: 'Nunito', sans-serif;font-size: 0.9rem;line-height: 1.6">
-    <div class="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'APILogger') }}
-                </a>
-            </div>
-        </nav>
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'APILogger') }}
+            </a>
+        </div>
+    </nav>
 
-        <main class="py-4">
-            <div class="container">
-                <div class="w-100 d-flex justify-content-between">
-                    <h3 class="text-center">Api Logger</h3>
-                    <form method="POST" action="{{ route('apilogs.deletelogs') }}">
-                        {{ csrf_field() }}
-                        {{ method_field('DELETE') }}
-                        <div class="form-group">
-                            <input type="submit" class="btn btn-danger delete-logs" value="Delete Logs">
-                        </div>
-                    </form>
+    <main class="p-4">
+        <div class="w-100 d-flex justify-content-between">
+            <h3 class="text-center">Api Logger</h3>
+            <form method="POST" action="{{ route('apilogs.deletelogs') }}">
+                {{ csrf_field() }}
+                {{ method_field('DELETE') }}
+                <div class="form-group">
+                    <input type="submit" class="btn btn-danger delete-logs" value="Delete Logs">
                 </div>
-                <div class="list-group">
-                    @forelse ($apilogs as $key => $log)
-                    <div class="list-group-item list-group-item-action" style="margin:5px">
-                        <div class = "row w-100">
-                            <span class="col-md-3">
-                                @if ($log->response>400)
-                                    <button class="btn btn-danger font-weight-bold">{{$log->method}}</button>
-                                @elseif($log->response>300)
-                                    <button class="btn btn-info font-weight-bold">{{$log->method}}</button>
-                                @else
-                                    <button class="btn btn-{{$log->method=="GET"? "primary" : "success"}} font-weight-bold">{{$log->method}}</button>
-                                @endif
-                                
-                                <small class="col-md-2">
-                                    <b>{{$log->response}}</b>
-                                </small>
-                            </span>
-                            <large class= "col-md-3"><b>Duration : </b>{{$log->duration * 1000}}ms</large>
-                            <large class= "col-md-3"><b>Date : </b>{{$log->created_at}}</large>
-                            <p class="col-md-3 mb-1"><b>IP :</b> {{$log->ip}}</p>
-                        </div>
-                        <hr>
-                        <div class="row w-100">
-                            <p class="col-md-3 mb-1">
-                                <b>URL : </b>{{$log->url}}</br>
-                            </p>
-                            <p class="col-md-6 mb-1"><b>Models(Retrieved) :</b> {{$log->models}}</p>
-                        </div>
-                        <div class="row w-100">
-                                <p class="col-md-3">
-                                    <b>Method :</b>   {{$log->action}}
-                                </p>
-                                <p class="col-md-3 mb-1"><b>Payload : </b>{{$log->payload}}</p>
+            </form>
+        </div>
+        <div class="list-group">
+            @forelse ($apilogs as $key => $log)
+            <div class="list-group-item list-group-item-action my-3">
+                <div class="row w-100" style="border-bottom: 2px solid lightgray">
+                    <span class="col-md-3">
+                        @if ($log->response>=400)
+                            <button class="btn btn-danger font-weight-bold">{{$log->method}}</button>
+                        @elseif($log->response>=300)
+                            <button class="btn btn-info font-weight-bold">{{$log->method}}</button>
+                        @else
+                            <button class="btn btn-{{$log->method=="GET"? "primary" : "success"}} font-weight-bold">{{$log->method}}</button>
+                        @endif
 
-                                <p class="col-md-6">
-                                    <b>Controller :</b> {{$log->controller}}
-                                    
-                                </p>
-                            
-                        </div>
+                        <small class="col-md-2">
+                            <b>{{$log->response}} - {{url($log->url)}}</b>
+                        </small>
+                    </span>
+                    <div class="col-md-3">
+                        <p class="mb-0"><b>Duration : </b>{{$log->duration * 1000}}ms<br />
+                        <b>Models(Retrieved) :</b><br />{!! empty($log->models) ? 'None' : implode('<br />', explode(', ', $log->models)) !!}</p>
                     </div>
-                    @empty
-                    <h5>
-                      No Records
-                    </h5>
-                  @endforelse
-                
+                    <div class= "col-md-3">
+                        <p class="mb-0"><b>IP :</b> {{$log->ip}}<br />
+                        <b>Date : </b>{{$log->created_at}}</p>
+                    </div>
+                    <div class="col-md-3 mb-1">
+                        <p class="mb-0"><b>Controller :</b> {{ empty($log->controller) ? 'None' : $log->controller }}<br />
+                        <b>Method :</b>  {{ empty($log->action) ? 'Closure' : $log->action }}</p>
+                    </div>
+                </div>
+                <h5 class="mt-3">Payload:</h5>
+                <div class="w-100">
+                     @dump(json_decode($log->payload, true))
                 </div>
             </div>
-        </main>
-    </div>
+            @empty
+            <h5>
+              No Records
+            </h5>
+          @endforelse
+
+        </div>
+    </main>
 </body>
 </html>
 
